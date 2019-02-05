ef633886e34442bde4185f3d11a82c575215a618
Anna Van Gils <vangils.anna@gmail.com>
2019-02-05 12:57:53 +0100

    backup old run based on version

--- run.sh
+++ run.sh
@@ -26,7 +26,7 @@ then
         if [ "$argument" == "cleanup" ]
         then
             # synchronize directory with on s3 (make sure you provision aws command lines (pip install aws-cli))
-            aws s3 sync /data/input/ "s3://$s3bucket/$uuid" --exact-time
+            aws s3 sync /data/input/ "s3://$s3bucket/data/dynamic/$uuid" --exact-time
 
             # Capture sync status
             SYNC_STATUS=$?
@@ -38,10 +38,10 @@ then
             ls -lhR /data/input/ > /data/EFStree.log
 
             # write tree log of the S3 directory
-            aws s3 ls  "s3://$s3bucket/$uuid" --recursive --summarize > /data/S3tree.log
+            aws s3 ls  "s3://$s3bucket/data/dynamic/$uuid" --recursive --summarize > /data/S3tree.log
 
             # write temporary tree log of S3 simulation directory
-            aws s3 ls  "s3://$s3bucket/$uuid/simulation" --recursive --summarize > /tmp/S3Simulationtree.log
+            aws s3 ls  "s3://$s3bucket/data/dynamic/$uuid/simulation" --recursive --summarize > /tmp/S3Simulationtree.log
 
             # capture number of files in the S3 Simulation directory
             J="$(grep  "^Total Objects:" /tmp/S3Simulationtree.log | grep -Eo "[0-9]+")"
@@ -59,20 +59,18 @@ then
             fi
 
             # sync logfile
-            aws s3 sync /data/ "s3://$s3bucket/$uuid/log" --exact-time --exclude "*" --include "*.log"
+            aws s3 sync /data/ "s3://$s3bucket/data/dynamic/$uuid/log" --exact-time --exclude "*" --include "*.log"
 
             # remove logfile
             rm -rf /data/*.log
 
         elif [ "$argument" == "rerun" ]
         then
-            # remove old backups
-            aws s3 rm "s3://$s3bucket/$uuid/backup/" --recursive
             # backup the old output except the simulation folder
-            aws s3 cp "s3://$s3bucket/$uuid/" "s3://$s3bucket/$uuid/backup/" --exclude "simulation/*" --exclude "backup/*" --recursive
+            aws s3 cp "s3://$s3bucket/data/dynamic/$uuid/" "s3://$s3bucket/data/dynamic/$uuid/backup/$version/" --exclude "simulation/*" --exclude "backup/*" --recursive
             # synchronize simulation folder from s3 back to the Elastic File System. Exclude ini file, this file is generated in do_docker_create task.
             echo "sync S3 to EFS"
-            aws s3 sync "s3://$s3bucket/$uuid/simulation" /data/output/simulation --exclude "input.ini" --exact-time
+            aws s3 sync "s3://$s3bucket/data/dynamic/$uuid/simulation" /data/output/simulation --exclude "input.ini" --exact-time
         fi
     done
 fi
