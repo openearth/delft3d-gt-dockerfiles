f92573cdc80994518ec102a0c8bc2c5892494a3b
Anna Van Gils <vangils.anna@gmail.com>
2019-02-12 16:26:01 +0100

    include simulation in backup

--- run.sh
+++ run.sh
@@ -67,7 +67,7 @@ then
         elif [ "$argument" == "rerun" ]
         then
             # backup the old output except the simulation folder
-            aws s3 cp "s3://$s3bucket/data/dynamic/$uuid/" "s3://$s3bucket/data/dynamic/$uuid/backup/$version/" --exclude "simulation/*" --exclude "backup/*" --recursive
+            aws s3 cp "s3://$s3bucket/data/dynamic/$uuid/" "s3://$s3bucket/data/dynamic/$uuid/backup/$version/" --exclude "backup/*" --recursive
             
             # synchronize simulation folder from s3 back to the Elastic File System.
             aws s3 sync "s3://$s3bucket/data/dynamic/$uuid/simulation" "/data/output/simulation" --exact-time
